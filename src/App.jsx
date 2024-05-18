7a3e0700f404040ac4291163cdc826639d7cc242
stUpGrade <160643466+stUpGrade@users.noreply.github.com>
2024-05-18 14:07:51 +0200

    maleteo

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        
+
+        <Routes>
+          
+          <Route>
+
+          </Route>
+
+        </Routes>
+
+
+      </Router>
+    </>
+  )
+}
+
+export default App
